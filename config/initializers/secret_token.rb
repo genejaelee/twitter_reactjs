40c233f06517ca5165f37936e806ee5a1191deb4
Gene Lee <genejaelee@gmail.com>
2015-01-22 12:32:06 -0800

    first commit, sessionscontroller, user model, pg db, oauth sign in, env variables

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwitterApp::Application.config.secret_key_base = '6b0a98f275b1ae3ca04b936040201d6d8978cc4cd5675c10b26af637a691c163bc3097660d16678c2cacca9af6899d926a5b193e788cb54bb81cd45de9cfc42d'
